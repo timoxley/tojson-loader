9321a55955bdd0ddd3b7a9481a54a8670928cbe7
romash1408 <romash1408@yandex.ru>
2017-12-20 17:29:41 +0700

    Add async execution ability

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 'use strict'
 
 module.exports = function toJSONLoader (code) {
-  return 'module.exports = ' + JSON.stringify(this.exec(code, this.resourcePath)) + '\n'
+  var result = this.exec(code, this.resourcePath)
+
+  if ('function' !== typeof result) {
+    return toJSON(result)
+  }
+
+  var callback = this.async()
+  if (!callback) {
+    if (result.sync) {
+      if ('function' === typeof result.sync) {
+        return toJSON(result.async())
+      }
+      else {
+        return toJSON(result.async)
+      }
+    }
+    
+    this.emitError('Resource ' + this.resourcePath + ' needs async but it is not supported')
+  }
+
+  result(function asyncCallback (result) {
+    callback(null, toJSON(result))
+  })
 }
 
 module.exports.pitch = function pitch () {
   this.clearDependencies()
+}
+
+function toJSON (result) {
+  return 'module.exports = ' + JSON.stringify(result) + '\n'
 }
